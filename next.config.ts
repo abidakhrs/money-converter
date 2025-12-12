cd05475c17d0074e33185aed60bc0d185c269d0b
abidakhrs <116633931+abidakhrs@users.noreply.github.com>
2025-12-13 00:08:07 +0800

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/money-converter',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
